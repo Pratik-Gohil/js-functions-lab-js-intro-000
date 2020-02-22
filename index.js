9f625ec8479fc5f6c19e45309d98811ac265eee1
Pratik <pr.tikgohil@gmail.com>
2020-02-22 11:06:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function happyHolidays() {
 function happyHolidaysTO(name) {
   return `Happy holidays ${name}!`
 }
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday} ${name}!`
+}
